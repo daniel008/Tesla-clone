5e2413d3b60549b869c8f8b6c1ee473e751d6378
DanielMa <mawenliang000@gmail.com>
2021-09-10 01:34:05 +0800

    Change background image to responsive.

--- src/components/Section.js
+++ src/components/Section.js
@@ -38,7 +38,6 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url('/images/model-s.jpg');
   display: flex;
   flex-direction: column;
   justify-content: space-between;
